c3d83bb1bdb230819661d6e978fb01322fe0a698
circlesoftmedia <circlesoftmedia@gmail.com>
2015-01-19 13:48:29 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nmi::Application.config.secret_token = '47b47d5d7c4bc509a68c5cc13291cf5b8bda37d94fd6ae59c90e7c726bb92e3dbd802b290db49df329a34b95f10f6515088da9979bb61afa7dd8c1a5ce2cbea7'
