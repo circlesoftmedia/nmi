e776bdbfd6e982d657177f3398b36f5c4f958600
shashi-we <shashi.we@gmail.com>
2015-03-23 11:34:31 +0530

    fixed migration error

--- db/migrate/20150322035658_add_user_id_to_products.rb
+++ db/migrate/20150322035658_add_user_id_to_products.rb
@@ -1,5 +1,6 @@
 class AddUserIdToProducts < ActiveRecord::Migration
   def change
+    remove_column :products, :brand_id
     add_column :products, :user_id, :integer
     add_column :products, :brand_id, :integer
   end
