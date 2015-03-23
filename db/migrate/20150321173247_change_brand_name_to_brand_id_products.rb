9d9e54d0ea08b29001df751282d8d336b0d4738d
shashi-we <shashi.we@gmail.com>
2015-03-23 11:23:05 +0530

    fixed migration error

--- db/migrate/20150321173247_change_brand_name_to_brand_id_products.rb
+++ db/migrate/20150321173247_change_brand_name_to_brand_id_products.rb
@@ -1,6 +1,6 @@
 class ChangeBrandNameToBrandIdProducts < ActiveRecord::Migration
   def up
     rename_column :products, :brand_name, :brand_id
-    change_column :products, :brand_id, :integer
   end
+
 end
--- db/migrate/20150322035658_add_user_id_to_products.rb
+++ db/migrate/20150322035658_add_user_id_to_products.rb
@@ -1,6 +1,6 @@
 class AddUserIdToProducts < ActiveRecord::Migration
   def change
     add_column :products, :user_id, :integer
-
+    change_column :products, :brand_id, :integer
   end
 end
